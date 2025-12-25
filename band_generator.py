5af2eae356f35cfa569fc7537b9be325ed2ce82c
MariaShaik. <mariashaik.ms@gmail.com>
2025-12-25 15:23:30 +0530

    Add files via upload

---
+++ band_generator.py
@@ -0,0 +1,4 @@
+print("welcome to the brand generator")
+city=input("whats the name of the city you grew up in?\n")
+pet=input("what is your pet name?\n")
+print("Your brand name could be:",city,pet)
